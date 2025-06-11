ec501530570c778e160a37669272cb67773daba6
AnaDiaz93 <anayancydiaz5@gmail.com>
2025-06-10 19:27:14 -0600

    actualizacion javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    //1.Seleccionar los elementos del DOM
+    //1. Seleccionar los elementos del DOM
     const registroForm = document.getElementById('registroForm');
     const nombreInput = document.getElementById('nombre');
     const emailInput = document.getElementById('correo');
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded',()=>{
     const errorEmail = document.getElementById('errorCorreo');
     const errorPassword = document.getElementById('errorPassword');
     const errorConfirmPassword = document.getElementById('errorConfirmPassword');
-    const successMessage = document.getElementById('successMessage');
+    const successMessage = document.getElementById(`successMessage`);
 
 
-    //2.Función para mostrar un mensaje de error
+    //Funcion para mostrar un mensaje de error
 
     function mostrarError(elementoError, inputElement, mensaje){
         elementoError.textContent = mensaje;
@@ -21,75 +21,70 @@ document.addEventListener('DOMContentLoaded',()=>{
         inputElement.classList.add('invalid');
     }
 
-    //3.Función para ocultar un mensaje de error
+    //Funcion para ocultar un mensaje de error
 
-    function ocultarError(elementoError, inputElement){                                                                
+    function ocultarError(elementoError, inputElement){
         elementoError.style.display = 'none';
-        inputElement.classList.remove('invalid');  
+        inputElement.classList.remove('invalid');
     }
-    
-    //4.Manejar el evento del submit del formulario
-    registroForm.addEventListener('submit', (event)=>{
+
+    //Manejar  el evento del submit del formulario
+    registroForm.addEventListener('submit',(event)=>{
         event.preventDefault();
         successMessage.style.display = 'none';
 
-        //Ocultar al enviarse el formulario
-        ocultarError(errorNombre, nombreInput);
-        ocultarError(errorEmail, emailInput);
-        ocultarError(errorPassword, passwordInput);
-        ocultarError(errorConfirmPassword, confirmPasswordInput);
+        ocultarError(errorNombre,nombreInput);
+        ocultarError(errorEmail,emailInput);
+        ocultarError(errorPassword,passwordInput);
+        ocultarError(errorConfirmPassword,confirmPasswordInput);
 
         //Validar todos los campos al enviar
+
         let formValido = true;
+
         if (nombreInput.value.trim() === ''){
             mostrarError(errorNombre,nombreInput,'El nombre es requerido');
             formValido = false;
         }
-
-        //Validación para correo electrónico
+        //Validacion para correo electronico
 
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
         if (emailInput.value.trim() === ''){
             mostrarError(errorEmail,emailInput,'El correo es requerido');
             formValido = false;
-        } 
-        else if (!emailPattern.test(emailInput.value.trim())){
-            mostrarError(errorEmail.emailInput,'Ingrese un correo válido');
+        }else if (!emailPattern.test(emailInput.value.trim())){
+            mostrarError(errorEmail,emailInput,'Ingrese un correo valido');
             formValido = false;
         }
-
-        //Validación de la contraseña    
+        //Validacion de la contraseña
         if (passwordInput.value.trim() === ''){
-            mostrarError(errorPassword,passwordInput, 'La contraseña es requerida');
+            mostrarError(errorPassword,passwordInput,'la contraseña es requerida');
+            formValido = false;
+
+        }else if (passwordInput.value.trim().length <6){
+            mostrarError(errorPassword,passwordInput,'La contreseña debe de ser mayor a 6');
             formValido = false;
         }
-        else if (passwordInput.value.trim().length < 6){
-            mostrarError(errorPassword,passwordInput, 'La contraseña debe ser mayor a 6 caracteres');
-            formValido = false;
-        }  
-        
-        //Validación de la confirmación de la contraseña
 
-         if (confirmPasswordInput.value.trim() === ''){
-            mostrarError(errorConfirmPassword,confirmPasswordInput, 'Debe de reingresar la contraseña');
+        //Validacion de la confirmacion de la contraseña
+
+        if (confirmPasswordInput.value.trim() === ''){
+            mostrarError(errorConfirmPassword,confirmPasswordInput,'Debe de reingresar la contraseña');
             formValido = false;
-         }
-         else if (confirmPasswordInput.value !== passwordInput.value){
+        }else if (confirmPasswordInput.value !== passwordInput.value){
             mostrarError(errorConfirmPassword,confirmPasswordInput,'Las contraseñas deben ser iguales');
             formValido = false;
-         }
+        }
 
-         // Si el formulario es válido, mostrar el mensaje de éxito y resetear el formulario
+        //Si el formulario es valido, mostrar el mensaje de exito y resetear el formulario
 
-         if (formValido){
+        if (formValido) {
             successMessage.style.display = 'block';
-            registroForm.reset();//Limpia todos los campos del formulario
-            console.log('¡Formulario enviado correctamente!');
-         }
+            registroForm.reset(); // Limpia todos los campos del formulario
+            console.log('Formulario enviado correctamente !');
 
+        }
 
     });
-
 
 });
